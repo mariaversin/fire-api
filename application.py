0a19592daf09fa261f9e7826ebc18b8d2b893b20
maria <verdejosinmaria@gmail.com>
2020-01-28 13:59:45 +0100

    deployed

---
+++ application.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask import Flask
+import src.predict  as p
+import os
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = '/home/maria/Documents/final-project/fotos'
+app.config['DEBUG'] = True
+
+@app.route('/fotos/<path:path>')
+def send_js(path):   
+ return send_from_directory('fotos', path)
+
+@app.route("/")
+def upload_file():
+ return render_template('index.html')
+
+@app.route("/upload", methods=['POST'])
+def uploader():
+  if request.method == 'POST': 
+    f = request.files['archivo']
+    filename = secure_filename(f.filename)
+
+    #f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #prediction = p.get_prediction(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    image = f.save("/home/maria/Documents/final-project/fotos/predict.jpg")
+    pred = p.get_prediction("/home/maria/Documents/final-project/fotos/predict.jpg")
+    return render_template("predict.html",pred=pred )
+
+if __name__ == '__main__':
+ app.run()
---
+++ src/predict.py
@@ -0,0 +1,55 @@
+import json
+import pandas as pd
+import numpy as np
+import cv2
+from PIL import Image
+import keras
+from keras import backend as k
+from keras.models import Sequential
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+from flask import request
+from flask import Flask
+from keras.models import model_from_json
+import os
+from tensorflow import keras
+import subprocess
+
+#def get_model():
+
+with open ('/home/maria/Documents/final-project/Model_0.97_150x150.json','r+') as f:
+    model_json = json.load(f)
+model = keras.models.model_from_json(model_json)
+model.load_weights('/home/maria/Documents/final-project/Model_0.97_150x150.h5')
+
+
+def preprocess_image(path):
+    im = cv2.imread(path)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(im,(150,150))
+    print(img.shape)
+    img = np.array(img)
+    print(img.shape)
+    img = np.expand_dims(img,axis=0)
+    print(img.shape)
+    return img
+
+def get_prediction(path):
+    prediction = model.predict(preprocess_image(path))[0]
+    subprocess.call(['speech-dispatcher'])  
+    clases = ['Fire', 'Neutral']
+    for i, p in enumerate(prediction):
+        if p > 0.5:
+            dicc =  {
+                "classes":clases,
+                "prediction": {
+                    "label":clases[i],
+                    "prob": str(p)}}
+            if 'Fire' in dicc['prediction']['label']:
+                subprocess.call(['spd-say', '"BE CAREFUL, FIRE!"'])
+            else:
+                subprocess.call(['spd-say', '"ALL RIGHT!"'])
+    return dicc if clases == 'Neutral' else dicc
+    
+
+    
